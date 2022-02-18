3e91c39ea31b735df5699998435d3266490f2abf
Sander Koenders <sanderkoenders@gmail.com>
2022-02-18 09:41:46 +0100

    Add ionic starter with react

---
+++ packages/nx-ionic-mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nx-ionic-mobile',
+  webDir: '../../dist/packages/nx-ionic-mobile',
+  bundledWebRuntime: false,
+};
+
+export default config;
